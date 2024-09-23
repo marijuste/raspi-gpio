f27813eb41222a1cffd1d13b13142198af8d69fc
Marius Hasler <marius.hasler@students.fhnw.ch>
2024-09-23 21:13:25 +0200

    add comments for actual gpio numbers

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 
 const gpos = [
-  { id: "1", gpo: new Gpio(536, "out") },
-  { id: "2", gpo: new Gpio(524, "out") },
-  { id: "3", gpo: new Gpio(537, "out") },
-  { id: "4", gpo: new Gpio(535, "out") },
+  { id: "1", gpo: new Gpio(536, "out") }, // 24
+  { id: "2", gpo: new Gpio(524, "out") }, // 13
+  { id: "3", gpo: new Gpio(537, "out") }, // 25
+  { id: "4", gpo: new Gpio(535, "out") }, // 23
 ];
 const PORT = 9091;
 const HOST = "0.0.0.0";
 
 server.on("message", (msg, rinfo) => {
-const rawMessage = msg.toString().split("\n");
-const message = rawMessage[0] ? rawMessage[0].split(":") : [];
+  const rawMessage = msg.toString().split("\n");
+  const message = rawMessage[0] ? rawMessage[0].split(":") : [];
 
   if (message.length === 3) {
     const gpoMessage = message[1];
     const stateMessage = message[2];
     gpos.forEach((gpo) => {
       if (gpo.id === gpoMessage) {
-        console.log(stateMessage === "ON" ? 1 : 0)
+        console.log(stateMessage === "ON" ? 1 : 0);
         gpo.gpo.writeSync(stateMessage === "ON" ? 1 : 0);
       }
     });
@@ -35,14 +35,12 @@ server.on("listening", () => {
   console.log(`Server listening on ${address.address}:${address.port}`);
 });
 
-
 function unexportOnClose() {
-
-    gpos.forEach((gpo)=>{
-        gpo.gpo.writeSync(0);
-        gpo.gpo.unexport();
-    })
+  gpos.forEach((gpo) => {
+    gpo.gpo.writeSync(0);
+    gpo.gpo.unexport();
+  });
 }
 
 server.bind(PORT, HOST);
-  process.on('SIGINT', unexportOnClose);
+process.on("SIGINT", unexportOnClose);
