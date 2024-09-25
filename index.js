0651b7977d6bf69156e8d52df17dde0547368d56
Marius Hasler <marius.hasler@gmx.net>
2024-09-25 11:05:59 +0200

    solved sysfs bug

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 
+const gpioMap = new Map([
+  ["1", { primary: 24, alt: 536 }],
+  ["2", { primary: 13, alt: 524 }],
+  ["3", { primary: 25, alt: 537 }],
+  ["4", { primary: 23, alt: 535 }],
+])
 const gpos = [
-  { id: "1", gpo: new Gpio(536, "out") }, // 24
-  { id: "2", gpo: new Gpio(524, "out") }, // 13
-  { id: "3", gpo: new Gpio(537, "out") }, // 25
-  { id: "4", gpo: new Gpio(535, "out") }, // 23
+  { id: "1", gpo: undefined }, // 24
+  { id: "2", gpo: undefined }, // 13
+  { id: "3", gpo: undefined }, // 25
+  { id: "4", gpo: undefined }, // 23
 ];
+try {
+  gpos.forEach((gpo) => {
+    gpo.gpo = new Gpio(gpioMap.get(gpo.id).primary, "out")
+  })
+
+} catch (error) {
+  console.error(`Error initializing GPIO in standard mode: ${error.message}  ..using alt numbers now.`);
+  gpos.forEach((gpo) => {
+    gpo.gpo = new Gpio(gpioMap.get(gpo.id).alt, "out")
+  })
+}
 const PORT = 9091;
 const HOST = "0.0.0.0";
 
